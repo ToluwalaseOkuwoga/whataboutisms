3de450f00e6b87ec14ae1ed5baa0189002b7b0a6
ToluwalaseOkuwoga <toluwalaseokuwoga@yahoo.com>
2024-07-05 10:47:07 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Animation on Scroll
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animate');
+        }
+    });
+});
+
+document.querySelectorAll('.about, .speaker').forEach(section => {
+    observer.observe(section);
+});
